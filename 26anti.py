154c11d61e3c922ec530bab37bb145dbbf98f3f8
JennaN23 <jennan@outlook.com>
2023-02-01 20:08:38 -0800

    updated

--- 26anti.py
+++ 26anti.py
@@ -1,20 +1,31 @@
 # 26anti.py
 
-# Write a program that prints the reverse-complement of a DNA sequence
-# You must use a loop and conditional
-
-# Variation: try this with the range() function and slice syntax
 
 dna  = 'ACTGAAAAAAAAAAA'
 
-for i in range(len(dna), -1, -1):
+# Write a program that prints the reverse-complement of a DNA sequence
+# You must use a loop and conditional
+
+rdna = dna[::-1]
+
+for nt in rdna:
+	if   nt == 'A': print('T',end='')
+	elif nt == 'C': print('G',end='')
+	elif nt == 'G': print('C',end='')
+	else:           print('A',end='')
+print()
+
+"""
+# Variation: try this with the range() function and slice syntax
+
+for i in range(len(dna), 0, -1):
 	nt = dna[i-1]
 	if   nt == 'A': print('T',end='')
 	elif nt == 'C': print('G',end='')
 	elif nt == 'G': print('C',end='')
-	else: print('A',end='')
+	else:           print('A',end='')
 print()
-	
+"""
 
 """
 python3 26anti.py
